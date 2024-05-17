1396d6e8fd2e388e00462c543dc7c29666c62ca8
Destiny4CODERS <132776933+soumeswar@users.noreply.github.com>
2024-05-17 23:25:19 +0530

    Update signer.bat

--- signer.bat
+++ signer.bat
@@ -1,3 +1,3 @@
 keytool -genkey -v -keystore my-release-key.jks -keyalg RSA -keysize 2048 -validity 10000 -alias my-key-alias
-apksigner sign --ks my-release-key.jks --out crackme_signed.apk crackme\dist\crackme.apk
-apksigner verify crackme_signed.apk
+apksigner sign --ks my-release-key.jks --out example.apk example\dist\example.apk
+apksigner verify example_signed.apk
